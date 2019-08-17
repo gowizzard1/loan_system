2fc6c6ad1b5c6fa45f0a264376ebe68a6b3ebed4
Isaac <ikiptanui@powergen-re.com>
2019-08-17 20:40:05 +0300

    add test

--- officers/admin.py
+++ officers/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from officers.models import Customer
+from officers.models import Agent
+from officers.models import Loan
+from officers.models import Officer
 
 # Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Agent)
+admin.site.register(Officer)
+admin.site.register(Loan)
--- officers/models.py
+++ officers/models.py
@@ -1,33 +1,49 @@
 from django.db import models
+
 import datetime
 # Create your models here.
 
-class Customers(models.Model):
+class Customer(models.Model):
     customer_name = models.CharField(max_length=100)
+    nationa_id = models.CharField(max_length=20)
     status = models.SmallIntegerField()
 
-class Agents(models.Model):
+    class Meta:
+        db_table = "customers"
+
+class Agent(models.Model):
     agent_name = models.CharField(max_length=100)
+    nationa_id = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20)
     status = models.IntegerField()
-    date_created = models.DateField(("Date"), default=datetime.date.today)  
+    date_created = models.DateField(("Date"), default=datetime.date.today) 
+
+    class Meta:
+        db_table = "agents"
 
 
-class Officers(models.Model):
+
+class Officer(models.Model):
     officer_name = models.CharField(max_length=100)
+    nationa_id = models.CharField(max_length=20)
     status = models.IntegerField()
     date_created = models.DateField(("Date"), default=datetime.date.today)
 
-class Loans(models.Model):
+    class Meta:
+        db_table = "officers"
+
+class Loan(models.Model):
     customer_name = models.CharField(max_length=100)
-    customer_id = models.ForeignKey(Customers,default=0,on_delete=models.SET_DEFAULT)
-    agent_id =models.ForeignKey(Agents,default=0,on_delete=models.SET_DEFAULT)
-    office_id = models.ForeignKey(Officers,default=0,on_delete=models.SET_DEFAULT)
+    customer_id = models.ForeignKey(Customer,default=0,on_delete=models.SET_DEFAULT)
+    agent_id =models.ForeignKey(Agent,default=0,on_delete=models.SET_DEFAULT)
+    office_id = models.ForeignKey(Officer,default=0,on_delete=models.SET_DEFAULT)
     amount = models.IntegerField()
     date_issued = models.DateField(("Date"), default=datetime.date.today)
     date_due = models.DateField(("Date"), default=datetime.date.today)
     interest = models.IntegerField()
 
-    def __str__(self):
-        return self.customer_name
+    class Meta:
+        db_table = "loans"
+
 
 
